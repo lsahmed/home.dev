785af07fdba5182e04fe6c59d68851068055bc50
Ethan <ethan@Ethans-MacBook-Pro.local>
2025-06-15 22:24:21 +0530

    Add use client

--- app/blog/page.jsx
+++ app/blog/page.jsx
@@ -1,3 +1,5 @@
+"use client"
+
 import Header from "@/components/header"
 import Footer from "@/components/footer"
 import BlogCard from "@/components/blog-card"
--- app/projects/page.jsx
+++ app/projects/page.jsx
@@ -1,3 +1,4 @@
+"use client"
 import Header from "@/components/header"
 import Footer from "@/components/footer"
 import ProjectCard from "@/components/project-card"
