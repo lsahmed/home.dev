2f1f97fb41ef494cef3cb60e95b865cf0638a285
Ahmed <md.ahmed701153@gmail.com>
2025-07-04 20:23:36 +0530

    Update route.js

--- app/api/route.js
+++ app/api/route.js
@@ -24,6 +24,15 @@ export async function GET() {
       demo: "https://demo.com",
       featured: false,
     },
+   {
+      title: "Web server with JWT authentication. 🏖️",
+      description:
+      "A Node.js web server with JWT-based authentication, featuring user registration and login routes. User data is securely stored in MongoDB with hashed passwords for enhanced security.",
+      image: "https://i.ibb.co/d4k7Tx5Q/webserverjwt.png",
+      tags: ["Node.js", "MongoDB", "Express" ,"JWT", "Bcrypt"],
+      github: "https://github.com/lsahmed/passwd",
+      featured: true,
+    },
     {
       title: "Password Manager",
       description:
@@ -38,7 +47,7 @@ export async function GET() {
       title: "Django Project with CRUD Functionality. [Contrail]",
       description:
       "With Contrail, users can create, read, update, and delete tweets. Each…Contrail is a simple web app for posting tweets, built with Django. With Contrail, users can create, read, update, and delete tweets. Each tweet can include a title, description, and an optional image. The app also has a strong focus on secure user authentication to keep accounts safe.",
-      image: "https://i.ibb.co/V0wSRcwm/Screenshot-2025-06-15-at-9-28-42-PM.png",
+      image: "https://i.ibb.co/39Cb3S82/crud.jpg",
       tags: ["Django", "SQL(via orm)", "Bootstrap" ,"Tailwind CSS", "Vercel"],
       github: "https://github.com/lsahmed/passwd",
       featured: false,
